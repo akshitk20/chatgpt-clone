377c3f3fe3e5f2321aaac539902a55dd4927669e
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-07-29 11:32:28 +0530

    initial commit

---
+++ src/main/java/com/springboot/chatgpt_clone/ChatgptCloneApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chatgpt_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/chatgpt_clone/ChatgptCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.chatgpt_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
